dc951e2e5bc82c6fd8e39599f52e049f167986f9
Alejandro Pérez Díaz <alejandro.perez.diaz@protonmail.com>
2026-06-24 12:22:48 +0200

    chore: update dependencies and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import { browserslistToTargets } from 'lightningcss'
+import browserslist from 'browserslist'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +10,12 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(
+        browserslist('Chrome >= 123, Firefox >= 120, Safari >= 17.5')
+      )
+    }
+  }
 })
