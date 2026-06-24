d94fa921b3fb907dd74a1bbab43e730abd48a6ad
Alejandro Pérez Díaz <alejandro.perez.diaz@protonmail.com>
2026-06-24 14:07:31 +0200

    chore: update vite.config.ts (target, cssTarget)

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  css: {
-    transformer: 'lightningcss',
-    lightningcss: {
-      targets: {
-        chrome: 123 << 16,
-        firefox: 120 << 16,
-        safari: (17 << 16) | (5 << 8),
-      }
-    }
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    cssTarget: 'safari18'
   }
-});
+})
